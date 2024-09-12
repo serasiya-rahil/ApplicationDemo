877b37325eb3c6ff6044e3e2158a96173d93b9fc
serasiya-rahil <serasiya.rahil@gmail.com>
2024-09-12 14:44:40 -0400

    Added comments in the file

--- ApplicationDemo/Models/Convertor.cs
+++ ApplicationDemo/Models/Convertor.cs
@@ -2,19 +2,20 @@
 
 namespace ApplicationDemo.Models
 {
-    public class Convertor
-    {
-        [Required(ErrorMessage = "Height is Required field")]
-        [Range(0.01, double.MaxValue)]
-        public double? HeightInCm { get; set; }
+	public class Convertor
+	{
+		[Required(ErrorMessage = "Height is Required field")]
+		[Range(0.01, double.MaxValue)]
+		public double? HeightInCm { get; set; }
 
-        public double? HeightInMeter { get; set; }
+		public double? HeightInMeter { get; set; }
 
-        public static double? CalculateHeightInMeters(double? height)
-        {
-            if (height == null) return null;
 
-            return height / 100;
-        }
-    }
+		public static double? CalculateHeightInMeters(double? height)
+		{
+			if (height == null) return null;
+
+			return height / 100;
+		}
+	}
 }
