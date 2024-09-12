abaa547fff204f4b626235a7b9751bbfde2b8319
serasiya-rahil <serasiya.rahil@gmail.com>
2024-09-12 14:43:33 -0400

    Add project files.

---
+++ ApplicationDemo/Controllers/ConvertController.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationDemo.Controllers
+{
+    public class ConvertController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Convertor());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Convertor convertor)
+        {
+            if (ModelState.IsValid)
+            {
+                convertor.HeightInMeter = Convertor.CalculateHeightInMeters(convertor.HeightInCm);
+            }
+            return View(convertor);
+        }
+    }
+}
---
+++ ApplicationDemo/Models/Convertor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationDemo.Models
+{
+    public class Convertor
+    {
+        [Required(ErrorMessage = "Height is Required field")]
+        [Range(0.01, double.MaxValue)]
+        public double? HeightInCm { get; set; }
+
+        public double? HeightInMeter { get; set; }
+
+        public static double? CalculateHeightInMeters(double? height)
+        {
+            if (height == null) return null;
+
+            return height / 100;
+        }
+    }
+}
