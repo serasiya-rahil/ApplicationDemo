48457b8ab63d681fc5b38207d7090175945605f6
serasiya-rahil <serasiya.rahil@gmail.com>
2024-09-12 15:07:51 -0400

    added comments in the controller

--- ApplicationDemo/Controllers/ConvertController.cs
+++ ApplicationDemo/Controllers/ConvertController.cs
@@ -3,22 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApplicationDemo.Controllers
 {
-    public class ConvertController : Controller
-    {
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View(new Convertor());
-        }
+	public class ConvertController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View(new Convertor());
+		}
 
-        [HttpPost]
-        public IActionResult Index(Convertor convertor)
-        {
-            if (ModelState.IsValid)
-            {
-                convertor.HeightInMeter = Convertor.CalculateHeightInMeters(convertor.HeightInCm);
-            }
-            return View(convertor);
-        }
-    }
+		[HttpPost]
+		public IActionResult Index(Convertor convertor)
+		{
+			//check Model State here
+			if (ModelState.IsValid)
+			{
+				//call the fucntion and convert the height
+				convertor.HeightInMeter = Convertor.CalculateHeightInMeters(convertor.HeightInCm);
+			}
+			return View(convertor);
+		}
+	}
 }
