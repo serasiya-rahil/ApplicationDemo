b41da8d0b5b78f0ece761b7cedcd9845779deb0a
serasiya-rahil <serasiya.rahil@gmail.com>
2024-09-13 00:05:09 -0400

    Fixed convertor.cs model return value, added label for tempconvertor.cshtml and home/index.cshtml files

--- ApplicationDemo/Models/Convertor.cs
+++ ApplicationDemo/Models/Convertor.cs
@@ -1,39 +1,41 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ApplicationDemo.Models
 {
-	public class Convertor
-	{
-		[Required(ErrorMessage = "Height is Required field")]
-		[Range(0.01, double.MaxValue)]
-		public double? HeightInCm { get; set; }
+    public class Convertor
+    {
+        [Required(ErrorMessage = "Height is Required field")]
+        [Range(0.01, double.MaxValue)]
+        public double? HeightInCm { get; set; }
 
-		public double? HeightInMeter { get; set; }
+        public double? HeightInMeter { get; set; }
 
-		//Process Height here
-		public static double? CalculateHeightInMeters(double? height)
-		{
-			if (height == null) return null;
+        //Process Height here
+        public static double? CalculateHeightInMeters(double? height)
+        {
+            if (height == null) return null;
 
-			return height / 100;
-		}
-	}
+            return height / 100;
+        }
+    }
 
-	public class Temperature
-	{
-		[Required(ErrorMessage = "Temp in Farenheit is Required")]
-		public double? TempInFarenheit { get; set; }
+    public class Temperature
+    {
+        [Required(ErrorMessage = "Temp in Farenheit is Required")]
+        [DisplayName("Temperature in Fahrenheit °F")]
+        public double? TempInFarenheit { get; set; }
 
-		public double? TempInCelcius { get; set; }
+        [DisplayName("Temperature in Celcius °C")]
+        public double? TempInCelcius { get; set; }
 
-		public static double? FarenheitToCelcius(double? TempInFarenheit)
-		{
-			if (TempInFarenheit == null) return null;
-			else
-			{
-				return ((TempInFarenheit - 32) * (5.0 / 9.0));
-
-			}
-		}
-	}
+        public static double? FarenheitToCelcius(double? TempInFarenheit)
+        {
+            if (TempInFarenheit == null) return null;
+            else
+            {
+                return Math.Round((double)((TempInFarenheit - 32) * (5.0 / 9.0)), 2);
+            }
+        }
+    }
 }
